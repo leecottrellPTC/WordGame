f16f5ff8baacbc9514f4d539bad318aa81a96bb3
Lee Cottrell <cottrell.lee@ptcollege.edu>
2019-02-04 11:00:13 -0500

    first push in lecture

---
+++ src/wordgame/WordGame.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package wordgame;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author lee_c
+ */
+public class WordGame {
+
+    /**
+     * @param args the command line arguments
+     */
+    private static Scanner keyboard = new Scanner(System.in);
+    private static String theWord;
+    public static void main(String[] args) {
+        // TODO code application logic here
+        //this is the code
+        System.out.println("Enter a word");
+        theWord = keyboard.nextLine();
+        System.out.println(theWord.trim());
+    }
+    
+}
